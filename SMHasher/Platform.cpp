7c665664fb8b6ff1b6ab8987e3614038850898ef
bulatz <bulat.ziganshin@gmail.com>
2015-05-28 18:53:34 +0300

    SMHasher: make Platform.cpp compatible with MINGW

--- SMHasher/Platform.cpp
+++ SMHasher/Platform.cpp
@@ -9,7 +9,7 @@ void testRDTSC ( void )
   printf("%d",(int)temp);
 }
 
-#if defined(_MSC_VER)
+#if defined(_WIN32)
 
 #include <windows.h>
 
@@ -27,11 +27,11 @@ void SetAffinity ( int /*cpu*/ )
 {
 #if !defined(__CYGWIN__) && !defined(__APPLE__)
   cpu_set_t mask;
-    
+
   CPU_ZERO(&mask);
-    
+
   CPU_SET(2,&mask);
-    
+
   if( sched_setaffinity(0,sizeof(mask),&mask) == -1)
   {
     printf("WARNING: Could not set CPU affinity\n");
