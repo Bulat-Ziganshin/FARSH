f521d0b3bc14583c23c0de8a51d1a3b802e03cc8
bulatz <bulat.ziganshin@gmail.com>
2015-05-29 21:34:04 +0300

    SMHasher: fixed 64-bit incompatibility in Platform.h

--- SMHasher/Platform.h
+++ SMHasher/Platform.h
@@ -75,7 +75,7 @@ __inline__ unsigned long long int rdtsc()
 #ifdef __x86_64__
     unsigned int a, d;
     __asm__ volatile ("rdtsc" : "=a" (a), "=d" (d));
-    return (unsigned long)a | ((unsigned long)d << 32);
+    return (unsigned long)a | ((unsigned long long)d << 32);
 #elif defined(__i386__)
     unsigned long long int x;
     __asm__ volatile ("rdtsc" : "=A" (x));
