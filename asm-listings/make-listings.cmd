aeb41ca9d25e8f2fee01a1fc3625fe0c68b31549
bulatz <bulat.ziganshin@gmail.com>
2015-05-29 11:37:24 +0300

    Simplified compilation scripts

--- asm-listings/make-listings.cmd
+++ asm-listings/make-listings.cmd
@@ -1,7 +1,5 @@
-set GCC=gcc -O3 -funroll-loops -s -static -c ../farsh.c
-::  -g
-%GCC% -O3 -funroll-loops -m32               -Wa,-adhlns=gcc-x86.lst
-%GCC% -O3 -funroll-loops -m32 -msse2 -DSSE2 -Wa,-adhlns=gcc-x86-sse2.lst
-%GCC% -O3 -funroll-loops -m32 -mavx2 -DAVX2 -Wa,-adhlns=gcc-x86-avx2.lst
-%GCC% -O3 -funroll-loops -m64 -msse2 -DSSE2 -Wa,-adhlns=gcc-x64.lst
-%GCC% -O3 -funroll-loops -m64 -mavx2 -DAVX2 -Wa,-adhlns=gcc-x64-avx2.lst
+gcc -O3 -funroll-loops -s -static -c ../farsh.c -O3 -funroll-loops -m32               -Wa,-adhlns=gcc-x86.lst
+gcc -O3 -funroll-loops -s -static -c ../farsh.c -O3 -funroll-loops -m32 -msse2 -DSSE2 -Wa,-adhlns=gcc-x86-sse2.lst
+gcc -O3 -funroll-loops -s -static -c ../farsh.c -O3 -funroll-loops -m32 -mavx2 -DAVX2 -Wa,-adhlns=gcc-x86-avx2.lst
+gcc -O3 -funroll-loops -s -static -c ../farsh.c -O3 -funroll-loops -m64 -msse2 -DSSE2 -Wa,-adhlns=gcc-x64.lst
+gcc -O3 -funroll-loops -s -static -c ../farsh.c -O3 -funroll-loops -m64 -mavx2 -DAVX2 -Wa,-adhlns=gcc-x64-avx2.lst
--- benchmark/compile.cmd
+++ benchmark/compile.cmd
@@ -1,6 +1,5 @@
-set GCC=gcc -O3 -funroll-loops -s -static
-%GCC% -m32               main.cpp -ofarsh-x86
-%GCC% -m32 -msse2 -DSSE2 main.cpp -ofarsh-x86-sse2
-%GCC% -m32 -mavx2 -DAVX2 main.cpp -ofarsh-x86-avx2
-%GCC% -m64 -msse2 -DSSE2 main.cpp -ofarsh-x64
-%GCC% -m64 -mavx2 -DAVX2 main.cpp -ofarsh-x64-avx2
+gcc -O3 -funroll-loops -s -static -m32               main.cpp -ofarsh-x86
+gcc -O3 -funroll-loops -s -static -m32 -msse2 -DSSE2 main.cpp -ofarsh-x86-sse2
+gcc -O3 -funroll-loops -s -static -m32 -mavx2 -DAVX2 main.cpp -ofarsh-x86-avx2
+gcc -O3 -funroll-loops -s -static -m64 -msse2 -DSSE2 main.cpp -ofarsh-x64
+gcc -O3 -funroll-loops -s -static -m64 -mavx2 -DAVX2 main.cpp -ofarsh-x64-avx2
