b744cf0209a0feb83f3ccbdb0f84d244490ac934
bulatz <bulat.ziganshin@gmail.com>
2015-05-31 12:27:25 +0300

    Always align key material at 16-byte bounds

--- farsh.h
+++ farsh.h
@@ -1,10 +1,10 @@
-/* Hash the buffer with the user-supplied key material */
+/* Hash the buffer with the user-supplied 16byte-aligned 1024-byte key material */
 unsigned farsh_keyed (const void *data, size_t bytes, const void *key);
 
-/* Hash the buffer with the user-supplied key material and return hash of 32*n bits long */
+/* Hash the buffer with the user-supplied 16byte-aligned, 1008+n*16 bytes long key material and return hash of 32*n bits long */
 void farsh_keyed_n (const void *data, size_t bytes, const void *key, int n, void *hash);
 
-/* Hash the buffer and return hash of 32*n bits long (n<=32), starting with hash number 'k' */
+/* Hash the buffer and return hash 32*n bits long (n<=32), starting with hash number 'k' */
 void farsh_n (const void *data, size_t bytes, int k, int n, void *hash);
 
 /* Hash the buffer */
