4149fcedb5757199a793e72dab162edbcca11238
bulatz <bulat.ziganshin@gmail.com>
2015-06-07 20:16:36 +0300

    Updated listings & added x64-nosimd benchmarking version (stupid auto-vectorisation perfromed by GCC)

--- asm-listings/make-listings.cmd
+++ asm-listings/make-listings.cmd
@@ -1,5 +1,6 @@
-gcc -O3 -funroll-loops -s -static -c ../farsh.c               -Wa,-adhlns=gcc-x86.lst
-gcc -O3 -funroll-loops -s -static -c ../farsh.c -msse2 -DSSE2 -Wa,-adhlns=gcc-x86-sse2.lst
-gcc -O3 -funroll-loops -s -static -c ../farsh.c -mavx2 -DAVX2 -Wa,-adhlns=gcc-x86-avx2.lst
-gcc -O3 -funroll-loops -s -static -c ../farsh.c -msse2 -DSSE2 -Wa,-adhlns=gcc-x64.lst
-gcc -O3 -funroll-loops -s -static -c ../farsh.c -mavx2 -DAVX2 -Wa,-adhlns=gcc-x64-avx2.lst
+gcc -O3 -funroll-loops -s -static -c ../farsh.c -m32               -Wa,-adhlns=gcc-x86.lst
+gcc -O3 -funroll-loops -s -static -c ../farsh.c -m32 -msse2 -DSSE2 -Wa,-adhlns=gcc-x86-sse2.lst
+gcc -O3 -funroll-loops -s -static -c ../farsh.c -m32 -mavx2 -DAVX2 -Wa,-adhlns=gcc-x86-avx2.lst
+gcc -O3 -funroll-loops -s -static -c ../farsh.c -m64               -Wa,-adhlns=gcc-x64-nosimd.lst
+gcc -O3 -funroll-loops -s -static -c ../farsh.c -m64 -msse2 -DSSE2 -Wa,-adhlns=gcc-x64.lst
+gcc -O3 -funroll-loops -s -static -c ../farsh.c -m64 -mavx2 -DAVX2 -Wa,-adhlns=gcc-x64-avx2.lst
--- benchmark/compile.cmd
+++ benchmark/compile.cmd
@@ -1,5 +1,6 @@
 gcc -O3 -funroll-loops -s -static -m32               main.cpp -ofarsh-x86
 gcc -O3 -funroll-loops -s -static -m32 -msse2 -DSSE2 main.cpp -ofarsh-x86-sse2
 gcc -O3 -funroll-loops -s -static -m32 -mavx2 -DAVX2 main.cpp -ofarsh-x86-avx2
+gcc -O3 -funroll-loops -s -static -m64               main.cpp -ofarsh-x64-nosimd
 gcc -O3 -funroll-loops -s -static -m64 -msse2 -DSSE2 main.cpp -ofarsh-x64
 gcc -O3 -funroll-loops -s -static -m64 -mavx2 -DAVX2 main.cpp -ofarsh-x64-avx2
