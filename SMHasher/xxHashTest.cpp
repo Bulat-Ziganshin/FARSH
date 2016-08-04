35db9e4b42adfe4bcc4498fff627cc48a26289d1
bulatz <bulat.ziganshin@gmail.com>
2016-08-04 22:23:22 +0300

    ZZH: modified the previous optimization, making the resulting quality a bit better
    The first TwoBytes test almost failed for zzh32c with 1.59x result:
    
    Keyset 'TwoBytes' - up-to-4-byte keys, 652545 total keys
    Testing collisions   - Expected    49.57, actual    79.00 ( 1.59x)
    Testing distribution - Worst bias is the  16-bit window at bit  20 -
    0.244%

--- SMHasher/xxHashTest.cpp
+++ SMHasher/xxHashTest.cpp
@@ -147,9 +147,9 @@ tail:
                     v2 = update(v2, v3, PRIME32_2, PRIME32_5, XXH_get32bits(bEnd-4));  // unaligned access!
                     break;
 
-        case  4:    v1 += p[3];
-        case  3:    v2 += p[2];
-        case  2:    v3 += p[1];
+        case  4:    v1 += p[3] << 24;
+        case  3:    v2 += p[2] << 16;
+        case  2:    v3 += p[1] <<  8;
         case  1:    v4 += p[0];
 
         case  0:    break;
